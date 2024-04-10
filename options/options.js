01165a4a977a1a8140b9de7f361be80c38f4e27f
Crowdbotics <team@crowdbotics.com>
2024-04-10 02:00:45 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-scene-47437",
+  url: "https://blue-scene-47437.botics.co",
+  api: "https://blue-scene-47437.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
